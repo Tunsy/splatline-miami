cb40cc97018e83e6be8c55aa782a8d8cd1f12f18
Johnny Nguyen <jonthnngyn@gmail.com>
2016-10-26 00:23:49 -0700

    Guns make sounds. Create shotguns

---
+++ splatlinemiami/Assets/Scripts/Shotgun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shotgun : Weapon {
+
+    public int bulletsPerShot;
+    public int spreadRange;
+
+    public override void Shoot()
+    {
+        // Play the shooting sound effect
+        if (shootSound)
+            AudioSource.PlayClipAtPoint(shootSound, transform.position);
+
+        for (int i = 0; i < bulletsPerShot; i++)
+        {
+            // Convert the angle of the player to the velocity of the bullet and shoot it forward
+            GameObject currentBullet = (GameObject)Instantiate(bullet.gameObject, transform.position, GetComponentInParent<Transform>().transform.rotation);
+            Vector2 angle = Quaternion.AngleAxis(transform.rotation.eulerAngles.z + Random.Range(spreadRange * -1, spreadRange), Vector3.forward) * Vector3.up;
+            currentBullet.GetComponent<Rigidbody2D>().velocity = angle * currentBullet.GetComponent<Bullet>().bulletSpeed;
+        }
+    }
+
+}
--- splatlinemiami/Assets/Scripts/Weapon.cs
+++ splatlinemiami/Assets/Scripts/Weapon.cs
@@ -5,14 +5,17 @@ public class Weapon : MonoBehaviour {
 
     public Bullet bullet;
     public float fireCooldown;
+    public AudioClip shootSound;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public virtual void Shoot()
+    {
+        // Play the shooting sound effect
+        if(shootSound)
+            AudioSource.PlayClipAtPoint(shootSound, transform.position);
+
+        // Convert the angle of the player to the velocity of the bullet and shoot it forward
+        GameObject currentBullet = (GameObject)Instantiate(bullet.gameObject, transform.position, GetComponentInParent<Transform>().transform.rotation);
+        Vector2 angle = Quaternion.AngleAxis(transform.rotation.eulerAngles.z + Random.Range(-3f, 3f), Vector3.forward) * Vector3.up;
+        currentBullet.GetComponent<Rigidbody2D>().velocity = angle * currentBullet.GetComponent<Bullet>().bulletSpeed;
+    }
 }
