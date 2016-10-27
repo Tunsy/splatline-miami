c3606cc53b4428463e2ef15cd2436bac181b6c85
Johnny Nguyen <jonthnngyn@gmail.com>
2016-10-27 16:55:34 -0700

    Stub out explosions and reloading

---
+++ splatlinemiami/Assets/Scripts/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DealDamage()
+    {
+        // Look for all enemies in collider
+
+        // Deal damage to all enemies
+    }
+}
--- splatlinemiami/Assets/Scripts/Weapon.cs
+++ splatlinemiami/Assets/Scripts/Weapon.cs
@@ -6,16 +6,46 @@ public class Weapon : MonoBehaviour {
     public Bullet bullet;
     public float fireCooldown;
     public AudioClip shootSound;
+    public int maxBulletCount;
+    private int currentBulletCount;
+    private bool isReloading;
+
+
+    public void Start()
+    {
+        currentBulletCount = maxBulletCount;
+    }
 
     public virtual void Shoot()
     {
-        // Play the shooting sound effect
-        if(shootSound)
-            AudioSource.PlayClipAtPoint(shootSound, transform.position);
+        if (!isReloading)
+        {
+            // Play the shooting sound effect
+            if (shootSound)
+                AudioSource.PlayClipAtPoint(shootSound, transform.position);
 
-        // Convert the angle of the player to the velocity of the bullet and shoot it forward
-        GameObject currentBullet = (GameObject)Instantiate(bullet.gameObject, transform.position, GetComponentInParent<Transform>().transform.rotation);
-        Vector2 angle = Quaternion.AngleAxis(transform.rotation.eulerAngles.z + Random.Range(-3f, 3f), Vector3.forward) * Vector3.up;
-        currentBullet.GetComponent<Rigidbody2D>().velocity = angle * currentBullet.GetComponent<Bullet>().bulletSpeed;
+            // Convert the angle of the player to the velocity of the bullet and shoot it forward
+            GameObject currentBullet = (GameObject)Instantiate(bullet.gameObject, transform.position, GetComponentInParent<Transform>().transform.rotation);
+            Vector2 angle = Quaternion.AngleAxis(transform.rotation.eulerAngles.z + Random.Range(-3f, 3f), Vector3.forward) * Vector3.up;
+            currentBullet.GetComponent<Rigidbody2D>().velocity = angle * currentBullet.GetComponent<Bullet>().bulletSpeed;
+
+            // Reload if ammo count is 0
+            currentBulletCount--;
+            if (currentBulletCount <= 0)
+            {
+                Reload();
+            }
+        }
+    }
+
+    public void Reload()
+    {
+        //Start a timer
+
+        //Play a reload sound
+
+        //Restart bullet count
+
+        //Be able to shoot again after timer ends
     }
 }
