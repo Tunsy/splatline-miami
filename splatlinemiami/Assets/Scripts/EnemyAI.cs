5e612e2b8694a41e85898915b87a3a58670939eb
Johnny Nguyen <jonthnngyn@gmail.com>
2016-11-08 17:08:41 -0800

    Stub out enemy AI and enmy spawner class

---
+++ splatlinemiami/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    public Transform targetPosition;
+    private Vector3 targetCoords;
+
+	// Use this for initialization
+	void Start () {
+        targetCoords = targetPosition.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetCoords = targetPosition.transform.position;
+
+
+    }
+}
---
+++ splatlinemiami/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public float currentTimer;
+    public float maxTime;
+    public float minTime;
+
+	// Use this for initialization
+	void Start () {
+        currentTimer = Random.Range(minTime, maxTime);
+	}
+
+    //TODO Random position
+	
+	// Update is called once per frame
+	void Update () {
+        currentTimer -= Time.deltaTime;
+
+        if(currentTimer <= 0 )
+        {
+            Instantiate(enemy, new Vector3(0, 0, 0), Quaternion.identity);
+            currentTimer = Random.Range(minTime, maxTime);
+        }
+	}
+}
